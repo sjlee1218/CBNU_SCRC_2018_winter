0b0cf906aeeee52dc37054000491fffa71f33e31
sjlee1218 <sjlee1218@postech.ac.kr>
2019-01-08 11:41:21 +0900

    I added cmake project files at serial_ubuntu

---
+++ serial_ubuntu/CMakeFiles/serial_ubuntu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/serial_ubuntu.dir/ceSerial.o"
+  "CMakeFiles/serial_ubuntu.dir/conserial.o"
+  "serial_ubuntu.pdb"
+  "serial_ubuntu"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/serial_ubuntu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
