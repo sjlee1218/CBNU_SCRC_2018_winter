6c55b38c7980cba506f3f9914211f5ca3315b599
SengJoon Lee <sjlee1218@postech.ac.kr>
2019-01-15 13:04:07 +0900

    DeadReckoning library

---
+++ DeadReckonging/spline_temp/cmake-build-debug/CMakeFiles/spline_temp.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/leeseungjoon/Desktop/GitHub_SJLEE/CBNU_SCRC_2018_winter/DeadReckonging/spline_temp/CatmullRom.cpp" "/Users/leeseungjoon/Desktop/GitHub_SJLEE/CBNU_SCRC_2018_winter/DeadReckonging/spline_temp/cmake-build-debug/CMakeFiles/spline_temp.dir/CatmullRom.cpp.o"
+  "/Users/leeseungjoon/Desktop/GitHub_SJLEE/CBNU_SCRC_2018_winter/DeadReckonging/spline_temp/Curve.cpp" "/Users/leeseungjoon/Desktop/GitHub_SJLEE/CBNU_SCRC_2018_winter/DeadReckonging/spline_temp/cmake-build-debug/CMakeFiles/spline_temp.dir/Curve.cpp.o"
+  "/Users/leeseungjoon/Desktop/GitHub_SJLEE/CBNU_SCRC_2018_winter/DeadReckonging/spline_temp/Vector.cpp" "/Users/leeseungjoon/Desktop/GitHub_SJLEE/CBNU_SCRC_2018_winter/DeadReckonging/spline_temp/cmake-build-debug/CMakeFiles/spline_temp.dir/Vector.cpp.o"
+  "/Users/leeseungjoon/Desktop/GitHub_SJLEE/CBNU_SCRC_2018_winter/DeadReckonging/spline_temp/main.cpp" "/Users/leeseungjoon/Desktop/GitHub_SJLEE/CBNU_SCRC_2018_winter/DeadReckonging/spline_temp/cmake-build-debug/CMakeFiles/spline_temp.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ DeadReckonging/spline_temp/cmake-build-debug/CMakeFiles/spline_temp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spline_temp.dir/CatmullRom.cpp.o"
+  "CMakeFiles/spline_temp.dir/Curve.cpp.o"
+  "CMakeFiles/spline_temp.dir/main.cpp.o"
+  "CMakeFiles/spline_temp.dir/Vector.cpp.o"
+  "spline_temp.pdb"
+  "spline_temp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spline_temp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
